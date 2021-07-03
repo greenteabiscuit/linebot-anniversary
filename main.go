a889796445d0d04ab3f62865b8cfc15613592942
reishi mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-07-04 01:04:57 +0900

    fix

--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
+	"strconv"
+	"time"
 
-    "github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
-    // LINE Botクライアント生成する
-    // BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
-    bot, err := linebot.New(
-        os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-        os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
-    )
-    // エラーに値があればログに出力し終了する
-    if err != nil {
-        log.Fatal(err)
-    }
-    // テキストメッセージを生成する
-    message := linebot.NewTextMessage("hello, world")
-    // テキストメッセージを友達登録しているユーザー全員に配信する
-    if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-        log.Fatal(err)
-    }
+	// LINE Botクライアント生成する
+	// BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	t1 := time.Date(2018, time.February, 28, 12, 0, 0, 0, time.UTC)
+	t2 := time.Date(2018, time.March, 31, 12, 0, 0, 0, time.UTC)
+	tNow := time.Now()
+	anniversarySinceMet := strconv.Itoa(int(tNow.Sub(t1).Hours() / 24))
+	anniversarySinceStartRelationship := strconv.Itoa(int(tNow.Sub(t2).Hours() / 24))
+	// テキストメッセージを生成する
+	message := linebot.NewTextMessage(anniversarySinceMet, anniversarySinceStartRelationship)
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
 }
