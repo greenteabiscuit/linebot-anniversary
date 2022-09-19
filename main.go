6b54e0ce06a09172ae355c10ef09dcc22d3e8ae8
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2022-09-19 22:58:54 +0900

    delete met date

--- main.go
+++ main.go
@@ -20,11 +20,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	t1 := time.Date(2018, time.February, 28, 0, 0, 0, 0, time.UTC)
-	t2 := time.Date(2018, time.March, 4, 0, 0, 0, 0, time.UTC)
+	relationshipStartDate := time.Date(2018, time.March, 4, 0, 0, 0, 0, time.UTC)
 	tNow := time.Now()
-	anniversarySinceMet := strconv.Itoa(int(tNow.Sub(t1).Hours() / 24))
-	anniversarySinceStartRelationship := strconv.Itoa(int(tNow.Sub(t2).Hours() / 24))
+	anniversarySinceStartRelationship := strconv.Itoa(int(tNow.Sub(relationshipStartDate).Hours() / 24))
 	// テキストメッセージを生成する
 	message := linebot.NewTextMessage(anniversarySinceMet + " days since met, " + anniversarySinceStartRelationship + " days since started dating")
 	// テキストメッセージを友達登録しているユーザー全員に配信する
